e56cdd395b9e0a5aa7120ec83ab197cd491db40a
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-04 20:20:14 +0530

    dfd

---
+++ src/nestedforloop.java
@@ -0,0 +1,20 @@
+
+public class nestedforloop {
+
+	public static void main(String[] args) {
+		int arr[][]= {{1,2,2},{1,2,6},{4,2,2}};
+		
+		for(int i=0; i<3;i++) {
+			
+			for(int j=0;j<3;j++) {
+				
+				
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
