5af5ba47328311ead5c640c6b2360e8e9e48382b
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-04 12:37:10 +0530

    new v

---
+++ src/comparisonoperator.java
@@ -0,0 +1,24 @@
+
+public class comparisonoperator {
+
+	public static void main(String[] args) {
+		
+		int rahul= 45;
+		int pratik =5;
+		
+		if (rahul == pratik) {
+		System.out.println("rahul == pratik");
+		}
+		
+		if (rahul != pratik) {
+			System.out.println("rahul != pratik");
+			
+		
+		}
+		
+		if (rahul > pratik) {
+			System.out.println("rahul> pratik");
+		}
+	}
+
+}
