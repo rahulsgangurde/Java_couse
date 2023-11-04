dfb480689d6c654ad3889118d9817c1b011b3864
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-04 12:51:10 +0530

    lo

---
+++ src/logicalopert.java
@@ -0,0 +1,20 @@
+
+public class logicalopert {
+
+	public static void main(String[] args) {
+		
+		
+		int rahul= 45;
+		int pratik =5;
+		
+		if ((rahul==45) && (pratik==5)) {
+		System.out.println("rahul == pratik");
+		}
+
+		if ((rahul==445) ||(pratik==5)) {
+			System.out.println("rahul + pratik");
+		
+		}
+	}
+
+}
