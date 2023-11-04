b60c130085c8358cd8bb5f0df095c31caf4c3c2b
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-04 12:20:28 +0530

    latest

--- src/basic/basic_learn.java
+++ src/basic/basic_learn.java
@@ -3,7 +3,7 @@ package basic;
 public class basic_learn {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/hhh/dfdf.java
+++ src/hhh/dfdf.java
@@ -5,9 +5,13 @@ public class dfdf {
 	public static void main(String[] args) {
 		int i= +1;
 		i++;
+		
 		System.out.println(i);
 		
-		
+		++i;
+		System.out.println(i);
+		System.out.println(i++);
+		System.out.println(i);
 	}
 
 }
