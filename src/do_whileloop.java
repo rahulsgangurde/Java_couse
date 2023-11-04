e5b2af54448c613126d2c3c59f8b56d883c88c81
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-04 19:16:28 +0530

    g

---
+++ src/do_whileloop.java
@@ -0,0 +1,14 @@
+
+public class do_whileloop {
+
+	public static void main(String[] args) {
+		
+		int count=1;
+		
+		do {
+		System.out.println("count is: " +count);
+		count++;
+		}while (count<15);
+	}
+
+}
