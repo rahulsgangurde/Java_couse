de25db5b6a0f1b50e2e6533057d6fd3e655d283a
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-04 14:29:03 +0530

    hshs

---
+++ src/ifelse.java
@@ -0,0 +1,22 @@
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		
+		int testscore = 85;
+		char mark;
+		
+		if(testscore>= 95){
+		   mark = "C";
+			
+			System.out.println("go boy");
+		}
+		else
+		{
+			System.out.println("wait boy");
+		}
+		
+		System.out.println("Grade= " +marks);
+	
+	}
+}
---
+++ src/nested_if.java
@@ -0,0 +1,21 @@
+
+public class nested_if {
+
+	public static void main(String[] args) {
+		int i= 45;
+		
+		if (i==45) {
+			System.out.println("My output is wrong");
+			if(i<50) {
+				System.out.println("My output is right");
+			if(i>=45) {
+				
+				System.out.println("My output is beauty");
+			}
+			
+			}
+		}
+
+	}
+
+}
