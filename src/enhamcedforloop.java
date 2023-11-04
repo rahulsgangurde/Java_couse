501d3e33fd1933cef792b63138f8cd6325923853
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-04 19:59:03 +0530

    bx

---
+++ src/enhamcedforloop.java
@@ -0,0 +1,16 @@
+
+public class enhamcedforloop {
+
+	public static void main(String[] args) {
+	
+		int[] numbers= {1,2,3,4,5,6,7,8,9,};
+		
+		for(int i :numbers) {
+			//use collectiom as numbers then represent collection
+			
+			System.out.println("count is:" +i);
+		}
+		
+	}
+
+}
