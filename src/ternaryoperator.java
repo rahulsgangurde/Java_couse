7b394e648440fd34a7c50aba7f1e79fe7ecd89c5
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-04 14:38:47 +0530

    nt

---
+++ src/ternaryoperator.java
@@ -0,0 +1,14 @@
+
+public class ternaryoperator {
+
+	public static void main(String[] args) {
+		int a= 45;
+		int b= 50;
+		int result;
+		
+		result=a>b?a:b;
+		System.out.println(result);
+
+	}
+
+}
