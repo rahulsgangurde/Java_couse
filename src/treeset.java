47add8c1146d652f5908a168d8bb0f22e6e9d6c7
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-05 12:41:49 +0530

    vsg

---
+++ src/treeset.java
@@ -0,0 +1,18 @@
+import java.util.TreeSet;
+
+public class treeset {
+
+	public static void main(String[] args) {
+		
+		TreeSet<String> mySet = new TreeSet <>();
+		
+		mySet.add("Donky");
+		mySet.add("Apple");
+		mySet.add("Cat");
+		mySet.add("B");
+		mySet.add("B");
+		
+		System.out.println(mySet);
+	}
+
+}
