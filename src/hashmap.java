9e1d32300e8950f94bf55bebd1175eca9fee62cc
rahulsgangurde <rahulsgangurde26@gmail.com>
2023-11-05 20:20:13 +0530

    dhdf

---
+++ src/hashmap.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+public class hashmap {
+
+	public static void main(String[] args) {
+		
+		HashMap <String,Integer> map =new HashMap<>();
+		
+		map.put("Rahul", 45);
+		map.put("Virat", 18);
+		map.put("Rohit", 5);
+		map.put("Rohit", 5);
+
+		
+		System.out.println("Map is:" + map.size());
+		
+		System.out.println("Map is:" + map);
+		
+		if(map.containsKey("Rohit")) {
+			
+			Integer rohit =map.get("Rohit");
+			
+		System.out.println( "the vale of map:"  + rohit  );
+			
+		for (String key : map.keySet()) {
+			
+			System.out.println("Key: "+key+" , value:"  +map.get(key));
+			
+			
+		}
+		
+		}
+		
+		
+		
+	}
+
+}
